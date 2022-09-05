2f0face47fb79688727668ab05fa841184443513
mike <michaelwilsonsj@hotmail.com>
2022-09-05 19:56:05 -0400

    vite with react & started Register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Register = () => (
+    <div className="formContainer">
+        <div className="formWrapper">
+            <span className="logo">mini slack</span>
+            <span className="logo">Register</span>
+            <input type="text" placeholder='display name' />
+            <input type="email" placeholder='email' />
+            <input type="password" placeholder='password' />
+            <input type="file" />
+        </div>
+    </div>
+)
+export default Register
