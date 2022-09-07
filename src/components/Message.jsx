c72f40f15d30a4fb4ff9e7d0e31e799eded9cfd9
mike <michaelwilsonsj@hotmail.com>
2022-09-07 02:02:38 -0400

    red ui

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
     return (
-        <div>Message</div>
+        <div className='message'>
+            <div className="messageInfo">
+                <img src="https://images.pexels.com/photos/842548/pexels-photo-842548.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <span>just now</span>
+            </div>
+            <div className="messageContent">
+                <p>hello</p>
+                <img src="https://images.pexels.com/photos/842548/pexels-photo-842548.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            </div>
+        </div>
     )
 }
 
