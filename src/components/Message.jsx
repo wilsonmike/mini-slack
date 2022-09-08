7809a7c30022b399b04f27560a7666fe9a06b1c3
mike <michaelwilsonsj@hotmail.com>
2022-09-08 00:19:04 -0400

    edited webkit scrollbar

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Message = () => {
     return (
-        <div className='message'>
+        <div className='message owner'>
             <div className="messageInfo">
                 <img src="https://images.pexels.com/photos/842548/pexels-photo-842548.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                 <span>just now</span>
