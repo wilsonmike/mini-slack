5309f55dcc8fdd09c8ab8cbbb8dc2d453b44c60a
mike <michaelwilsonsj@hotmail.com>
2022-09-09 05:28:07 -0400

    firebase storage and firestore added (need to fix uncaught inpromise 'displayName' does not exist

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,23 +1,71 @@
+import { useState } from 'react';
 import { logo, upload } from '../assets'
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from '../firebase';
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
-const Register = () => (
-    <div className="formContainer">
-        <div className="formWrapper">
-            <img src={logo} alt="minislack logo" className='logo' />
-            <span className="title">Register</span>
-            <form>
-                <input type="text" placeholder='Display Name' />
-                <input type="email" placeholder='Email' />
-                <input type="password" placeholder='Password' />
-                <input style={{ display: "none" }} type="file" id="file" />
-                <label htmlFor="file">
-                    <img src={upload} alt="upload avatar" />
-                    <span>Add an avatar</span>
-                </label>
-                <button>Sign Up</button>
-            </form>
-            <p>Already have an account? Login</p>
+const Register = () => {
+    const [err, setErr] = useState(false)
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
+
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+            const storageRef = ref(storage, displayName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on(
+                (error) => {
+                    // Handle unsuccessful uploads
+                    setErr(true)
+                },
+                () => {
+                    // Handle successful uploads on complete
+                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                    getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+                        await updateProfile(res.user, {
+                            displayName,
+                            photoURL: downloadURL,
+                        });
+                        await setDoc(doc(db, "users", res.user.uid), {
+                            uid: res.user.uid,
+                            displayName,
+                            email,
+                            photoURL: downloadURL
+                        });
+                    });
+                }
+            );
+        } catch (error) {
+            setErr(true)
+        }
+
+    }
+    return (
+        <div className="formContainer">
+            <div className="formWrapper">
+                <img src={logo} alt="minislack logo" className='logo' />
+                <span className="title">Register</span>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder='Display Name' />
+                    <input type="email" placeholder='Email' />
+                    <input type="password" placeholder='Password' />
+                    <input style={{ display: "none" }} type="file" id="file" />
+                    <label htmlFor="file">
+                        <img src={upload} alt="upload avatar" />
+                        <span>Add an avatar</span>
+                    </label>
+                    <button>Sign Up</button>
+                    {err && <span>Oops, something went wrong.</span>}
+                </form>
+                <p>Already have an account? Login</p>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 export default Register
