9126e4ab23400394e11a54d817809d4cde7b8b0b
mike <michaelwilsonsj@hotmail.com>
2022-09-12 21:42:05 -0400

    post being blocked in firebase

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,46 +1,49 @@
-import { useState } from 'react';
-import { logo, upload } from '../assets'
+import React, { useState } from "react";
+import { logo, upload } from "../assets";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth, db, storage } from '../firebase';
+import { auth, db, storage } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate, Link } from "react-router-dom";
 
 const Register = () => {
-    const [err, setErr] = useState(false)
-    const [loading, setLoading] = useState(false)
-    const navigate = useNavigate()
+    const [err, setErr] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
-        setLoading(true)
-        e.preventDefault()
+        setLoading(true);
+        e.preventDefault();
         const displayName = e.target[0].value;
         const email = e.target[1].value;
         const password = e.target[2].value;
         const file = e.target[3].files[0];
 
         try {
-            // create user
+            //Create user
             const res = await createUserWithEmailAndPassword(auth, email, password);
-            // create unique name
+
+            //Create a unique image name
             const date = new Date().getTime();
             const storageRef = ref(storage, `${displayName + date}`);
 
             await uploadBytesResumable(storageRef, file).then(() => {
                 getDownloadURL(storageRef).then(async (downloadURL) => {
                     try {
+                        //Update profile
                         await updateProfile(res.user, {
                             displayName,
                             photoURL: downloadURL,
                         });
-                        // create user on fireb
+                        //create user on firestore
                         await setDoc(doc(db, "users", res.user.uid), {
                             uid: res.user.uid,
                             displayName,
                             email,
                             photoURL: downloadURL,
-                        })
-                        // create empty chats
+                        });
+
+                        //create empty user chats on firestore
                         await setDoc(doc(db, "userChats", res.user.uid), {});
                         navigate("/");
                     } catch (err) {
@@ -48,33 +51,38 @@ const Register = () => {
                         setErr(true);
                         setLoading(false);
                     }
-                })
+                });
             });
         } catch (err) {
             setErr(true);
             setLoading(false);
         }
     };
+
     return (
         <div className="formContainer">
             <div className="formWrapper">
-                <img src={logo} alt="minislack logo" className='logo' />
+                <img src={logo} alt="minislack logo" className="logo" />
                 <span className="title">Register</span>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder='Display Name' />
-                    <input type="email" placeholder='Email' />
-                    <input type="password" placeholder='Password' />
-                    <input style={{ display: "none" }} type="file" id="file" />
+                    <input required type="text" placeholder="display name" />
+                    <input required type="email" placeholder="email" />
+                    <input required type="password" placeholder="password" />
+                    <input required style={{ display: "none" }} type="file" id="file" />
                     <label htmlFor="file">
-                        <img src={upload} alt="upload avatar" />
+                        <img src={upload} alt="" />
                         <span>Add an avatar</span>
                     </label>
-                    <button>Sign Up</button>
-                    {err && <span className='errorMessage'>Oops, something went wrong. Please make sure your password is at least 8 characters.</span>}
+                    <button disabled={loading}>Sign up</button>
+                    {loading && "Uploading and compressing the image please wait..."}
+                    {err && <span>Something went wrong</span>}
                 </form>
-                <p>Already have an account? <Link to="/login">Login</Link></p>
+                <p>
+                    You do have an account? <Link to="/register">Login</Link>
+                </p>
             </div>
         </div>
-    )
-}
-export default Register
+    );
+};
+
+export default Register;
